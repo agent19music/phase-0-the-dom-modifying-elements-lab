f96d7b88f3aec5d0a46f5f388524457174da1273
seannyandusi <seanmotanya@gmail.com>
2023-09-26 14:35:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.querySelector('#main')
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = 'SEAN-NYANDUSI is the champion';
